022ba48d16345d1a224e36bbeca4b2aa10663541
Manoj Sharma <148334299+manoj1234-ms@users.noreply.github.com>
2025-08-06 21:32:39 +0530

    log file added

--- src/mcqgenerator/logger.py
+++ src/mcqgenerator/logger.py
@@ -2,19 +2,16 @@ import logging
 import os
 from datetime import datetime
 
-# create LOG_FILE that stores logging data
-LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 
-#get the path of folders
-log_path = os.path.join(os.getcwd(), "logs")
+log_path=os.path.join(os.getcwd(),"logs")
 
-#create a dir
-os.makedirs(log_path, exist_ok=True)
+os.makedirs(log_path,exist_ok=True)
 
-#join the dir with path
-LOG_FILE_Path = os.path.join(log_path, LOG_FILE)
+LOG_FILEPATH=os.path.join(log_path,LOG_FILE)
 
-logging.basicConfig(
-    level= logging.INFO,
-    format = "[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s"
+
+logging.basicConfig(level=logging.INFO, 
+                    filename=LOG_FILEPATH,
+                    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s"
 )
